34689ff495703b025191d8fa5f7752377d2a1bb1
陈东梅 <1174411878@qq.com>
2015-05-18 12:32:09 +0800

    5.18日分支同步

--- js/data.js
+++ js/data.js
@@ -1,14 +1,70 @@
 function business(){
-    var business = {"ImTimeSign":"77","ad":[{"act":0,"ad_charge":2,"adtd":1001300,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=0000000ThH3StWMCPN542p-bX3KhoIDNna9RyXOIzoraD3gHUBzrb-dolZUcOk2UMUVLI8d71G9ZkbVDKOPuwThO2j4h881M9PGBNl1E5uo09CHZKv2jgAOViv8RJMi4x-UvrNn.DR_jIy16CrwKZ63pMmcrSa9G4mLmFCR_Nn8ozTmm8Boo6CpXgih4Sjik8o9ksqT7jHzs_lTUQqRHAlEj4qhZdL3xy1j4en5ot_5VlX8a9G4mgwRDkRAPdXZ1en5oEsSVhZ1vmxg4mxUY4t5M_se5gj_S8Z1LmxgkseO5o6CpXy6hUik9tS1jlOQj_tt5MvmxgdsSEW9tS1jlSLj_SZj4qrZuEse5gj4en5ot_5VlNBmk_Lw3d9h9mlXMmC0.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":402070,"qk":"555062133fdaa50792f56a14","tit":"�ͼ۶��ֳ�","type":"text","w":0},{"act":0,"ad_charge":2,"adtd":1000700,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=000000jyDh26NL7WFrFO2ZWVyh6Lr0z6nmgPHoCouPTcr7nwMUANEkNMZFWzw-xP0sPqNG_v9t9Pfddx0pTbe3CtEkpNFkn1VUr5esZS7VJR8I5yjPjoDAY-uA3xUyt7aI4l6tm.DY_iFS9nNa5zWI6YmhgC6x7oIKg3eFStP7lmJjArZNsp888uv-BG-dXHWdBuYGm9Bl288FBVmhnUPYXPmuCyr1Gen-B6.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":404381,"qk":"555062133fdaa50792f56a14","tit":"1Ԫ����","type":"text","w":0},{"act":0,"ad_charge":2,"adtd":1000700,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=000000Km_OwbWG5AOfE2Tpp3Q-g7fObRtU3sbf5R4k8f5S9bMFLG2HwgTsdBkBoeiCImgFZH_gzrSQlB4yI356yWZDpuOoUfhR_oWHBrUjBuUmJCmQeRK9cWD8MEw5lNswiP2_t.DD_if_lTUQqRg-nePklUDfwGYsNqn77h24SUMLsRP5QAeKPa-BG-dXHWdsRP5QGHTOKGm9ksoDkWu8WkojPakklxZGlN.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":404466,"qk":"555062133fdaa50792f56a14","tit":"��360����","type":"text","w":0},{"act":0,"ad_charge":2,"adtd":1000700,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=00000009jU8IJTofx59dz73BmF_7_HsqN9bf7gt3Xqt7fhQy2R4jnS-n7PbK-zhLUcSe2cGbu4DF7HTizh0PV7BDXdBSRE3qS7cdYVQ6qiYU1jd5kN0Q3E_yADyJbEBbI885oHD.DY_NR2Ar5Od663rj6tVjMRgC6WHj6hHKwuB8xh6oLTStXFWF8l_3hcygde88a9G4mLmFCR_gLTVHQJtMZo6CpXgih4SjikxIzs33NBmk_Lw3d9h9mo3eJS26.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":404453,"qk":"555062133fdaa50792f56a14","tit":"����ֻ��50","type":"text","w":0},{"act":0,"ad_charge":2,"adtd":1000700,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=000000jbeR7pHMKgf7_kDLPNKh78FF0rMW3peoPDmtnaFeWbQasMpqmDBgxxW_tEOtmPggGrGklCH30kg8P-QK-XVR0bvH_NF7dMMDWKZg2QXF61bu9L3KdkurZDyUGg_FVu_Ic.7Y_iwt5-TzeCxwuwK6CpXyPvap7QZeeQJtMZo6CpXgih4SjiktM_kxeHtLgKfYt_QCJamJjZ4M4E1nE14r8a9G4myIrP-SJFW3ygtE3dF1xE3ZTEs_EOUH7OcX_HAPhl5A_nYQAHGolIz6.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":400431,"qk":"555062133fdaa50792f56a14","tit":"¡�ػ���","type":"text","w":0},{"act":0,"ad_charge":2,"adtd":1000700,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=000000a0Z0nOyYtktSpqA7_euVTHoLXWeRy09NKwdf_w6J1GiNdTzXGJzRdk-j27_tmDLeviBn7NIl7GTlXy3ylfjYrvi6PhXbQ89Ou6QN_gyUMd0xELJVG7nmD4HVNDpLWIZl6.DY_aWqDIH_ac2j_U3mrd5_xhoo6e_Lwl4MpphgN4rnQnHtXr7nN1PYpknmvUCtps1f_TxAEXC0.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":404854,"qk":"555062133fdaa50792f56a14","tit":"���ݲ�Ʒ","type":"text","w":0}],"city":"","n":6,"province":""};
+    var business = {"ImTimeSign":"77","ad":[{"act":0,"ad_charge":2,"adtd":1001300,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=0000000ThH3StWMCPN542p-bX3KhoIDNna9RyXOIzoraD3gHUBzrb-dolZUcOk2UMUVLI8d71G9ZkbVDKOPuwThO2j4h881M9PGBNl1E5uo09CHZKv2jgAOViv8RJMi4x-UvrNn.DR_jIy16CrwKZ63pMmcrSa9G4mLmFCR_Nn8ozTmm8Boo6CpXgih4Sjik8o9ksqT7jHzs_lTUQqRHAlEj4qhZdL3xy1j4en5ot_5VlX8a9G4mgwRDkRAPdXZ1en5oEsSVhZ1vmxg4mxUY4t5M_se5gj_S8Z1LmxgkseO5o6CpXy6hUik9tS1jlOQj_tt5MvmxgdsSEW9tS1jlSLj_SZj4qrZuEse5gj4en5ot_5VlNBmk_Lw3d9h9mlXMmC0.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":402070,"qk":"555062133fdaa50792f56a14","tit":"低价二手车","type":"text","w":0},{"act":0,"ad_charge":2,"adtd":1000700,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=000000jyDh26NL7WFrFO2ZWVyh6Lr0z6nmgPHoCouPTcr7nwMUANEkNMZFWzw-xP0sPqNG_v9t9Pfddx0pTbe3CtEkpNFkn1VUr5esZS7VJR8I5yjPjoDAY-uA3xUyt7aI4l6tm.DY_iFS9nNa5zWI6YmhgC6x7oIKg3eFStP7lmJjArZNsp888uv-BG-dXHWdBuYGm9Bl288FBVmhnUPYXPmuCyr1Gen-B6.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":404381,"qk":"555062133fdaa50792f56a14","tit":"1元理财","type":"text","w":0},{"act":0,"ad_charge":2,"adtd":1000700,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=000000Km_OwbWG5AOfE2Tpp3Q-g7fObRtU3sbf5R4k8f5S9bMFLG2HwgTsdBkBoeiCImgFZH_gzrSQlB4yI356yWZDpuOoUfhR_oWHBrUjBuUmJCmQeRK9cWD8MEw5lNswiP2_t.DD_if_lTUQqRg-nePklUDfwGYsNqn77h24SUMLsRP5QAeKPa-BG-dXHWdsRP5QGHTOKGm9ksoDkWu8WkojPakklxZGlN.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":404466,"qk":"555062133fdaa50792f56a14","tit":"融360贷款","type":"text","w":0},{"act":0,"ad_charge":2,"adtd":1000700,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=00000009jU8IJTofx59dz73BmF_7_HsqN9bf7gt3Xqt7fhQy2R4jnS-n7PbK-zhLUcSe2cGbu4DF7HTizh0PV7BDXdBSRE3qS7cdYVQ6qiYU1jd5kN0Q3E_yADyJbEBbI885oHD.DY_NR2Ar5Od663rj6tVjMRgC6WHj6hHKwuB8xh6oLTStXFWF8l_3hcygde88a9G4mLmFCR_gLTVHQJtMZo6CpXgih4SjikxIzs33NBmk_Lw3d9h9mo3eJS26.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":404453,"qk":"555062133fdaa50792f56a14","tit":"理财只需50","type":"text","w":0},{"act":0,"ad_charge":2,"adtd":1000700,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=000000jbeR7pHMKgf7_kDLPNKh78FF0rMW3peoPDmtnaFeWbQasMpqmDBgxxW_tEOtmPggGrGklCH30kg8P-QK-XVR0bvH_NF7dMMDWKZg2QXF61bu9L3KdkurZDyUGg_FVu_Ic.7Y_iwt5-TzeCxwuwK6CpXyPvap7QZeeQJtMZo6CpXgih4SjiktM_kxeHtLgKfYt_QCJamJjZ4M4E1nE14r8a9G4myIrP-SJFW3ygtE3dF1xE3ZTEs_EOUH7OcX_HAPhl5A_nYQAHGolIz6.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":400431,"qk":"555062133fdaa50792f56a14","tit":"隆重婚宴","type":"text","w":0},{"act":0,"ad_charge":2,"adtd":1000700,"bid":"","clklogurl":"","curl":"http://www.baidu.com/dianjin.php?url=000000a0Z0nOyYtktSpqA7_euVTHoLXWeRy09NKwdf_w6J1GiNdTzXGJzRdk-j27_tmDLeviBn7NIl7GTlXy3ylfjYrvi6PhXbQ89Ou6QN_gyUMd0xELJVG7nmD4HVNDpLWIZl6.DY_aWqDIH_ac2j_U3mrd5_xhoo6e_Lwl4MpphgN4rnQnHtXr7nN1PYpknmvUCtps1f_TxAEXC0.mgK85R4NHDs0mvYqn0K9UHYs0APsm1Yk0AdY5H00ULnq0Aw-IWdrNRkn0ZKzU1YznfKWIjY1rjD0mv6qn0K8ugfqnfKLpyuG5HD0mgKsTv-b5ymdnjP9rAfL0AN3TjdWIZFxTHYknH0sQAPsUNqk5HDsQA99U1DzndqWIZcqnBk_mdq-Th-1uyfqQ0K1pAqLgvN3TjY0Tv-b5HRdPH0vnWD1nvubmyDdnjT4nhmdPhDkP0KWThnqPWnLPs","desc":" ","h":0,"id":404854,"qk":"555062133fdaa50792f56a14","tit":"绿瘦产品","type":"text","w":0}],"city":"","n":6,"province":""};
     return business;
 }
 
 
 function goodsLoop(){
-    var goods = {"shopping":[{"company":"΢��","shCon":[{"img":"image/product.jpg","goodsName":"SMA ĸͷת ������1","leadTime":"��������1-7��","goodsPrice":"��399.00","goodsNum":"1","price":"��399.00"},{"img":"image/product.jpg","goodsName":"SMA ĸͷת ������2","leadTime":"��������1-7��","goodsPrice":"��399.00","goodsNum":"1","price":"��399.00"}]},{"company":"΢��2","shCon":[{"img":"image/product.jpg","goodsName":"SMA ĸͷת ������3","leadTime":"��������1-7��","goodsPrice":"��399.00","goodsNum":"1","price":"��399.00"},{"img":"image/product.jpg","goodsName":"SMA ĸͷת ������4","leadTime":"��������1-7��","goodsPrice":"��399.00","goodsNum":"1","price":"��399.00"}]}]};
+    var goods = {"shopping":[{"company":"微往","shCon":[{"img":"image/product.jpg","goodsName":"SMA 母头转 适配器1","leadTime":"供货周期1-7天","goodsPrice":"￥399.00","goodsNum":"1","price":"￥399.00"},{"img":"image/product.jpg","goodsName":"SMA 母头转 适配器2","leadTime":"供货周期1-7天","goodsPrice":"￥399.00","goodsNum":"1","price":"￥399.00"}]},{"company":"微往2","shCon":[{"img":"image/product.jpg","goodsName":"SMA 母头转 适配器3","leadTime":"供货周期1-7天","goodsPrice":"￥399.00","goodsNum":"1","price":"￥399.00"},{"img":"image/product.jpg","goodsName":"SMA 母头转 适配器4","leadTime":"供货周期1-7天","goodsPrice":"￥399.00","goodsNum":"1","price":"￥399.00"}]}]};
     return goods;
 }
 
+function getList(){
+    var list=[{name:"Angularjs脏检查$watch",link:"Angularjs.html"},
+        {name:"Angularjs加法机",link:"adder.html"},
+        {name:"Angularjs购物车",link:"shopping.html"},
+        {name:"Angularjs控制器嵌套",link:"controller-nested.html"},
+        {name:"Angularjs表达式",link:"expression.html"},
+        {name:"Angularjs计算器案例",link:"calculator.html"},
+        {name:"Canvas时钟",link:"clock.html"},
+        {name:"Angularjs字符串插入",link:"insert-string.html"},
+        {name:"Angularjs脏检查$watch1",link:"Angularjs.html"},
 
+        {name:"Angularjs加法机2",link:"adder.html"},
+        {name:"Angularjs购物车3",link:"shopping.html"},
+        {name:"Angularjs控制器嵌套4",link:"controller-nested.html"},
+        {name:"Angularjs表达式5",link:"expression.html"},
+        {name:"Angularjs计算器案例6",link:"calculator.html"},
+        {name:"Canvas时钟7",link:"clock.html"},
+        {name:"Angularjs字符串插入8",link:"insert-string.html"},
+        {name:"Angularjs脏检查$watch9",link:"Angularjs.html"},
+        {name:"Angularjs加法机10",link:"adder.html"},
+        {name:"Angularjs购物车11",link:"shopping.html"},
+        {name:"Angularjs控制器嵌套12",link:"controller-nested.html"},
+        {name:"Angularjs表达式13",link:"expression.html"},
+        {name:"Angularjs计算器案例14",link:"calculator.html"},
+        {name:"Canvas时钟15",link:"clock.html"},
+        {name:"Angularjs字符串插入16",link:"insert-string.html"},{name:"Angularjs脏检查$watch",link:"Angularjs.html"},
+        {name:"Angularjs加法机17",link:"adder.html"},
+        {name:"Angularjs购物车18",link:"shopping.html"},
+        {name:"Angularjs控制器嵌套19",link:"controller-nested.html"},
+        {name:"Angularjs表达式20",link:"expression.html"},
+        {name:"Angularjs计算器案例21",link:"calculator.html"},
+        {name:"Canvas时钟22",link:"clock.html"},
+        {name:"Angularjs字符串插入23",link:"insert-string.html"},
+        {name:"Angularjs计算器案例24",link:"calculator.html"},
+        {name:"Canvas时钟25",link:"clock.html"},
+        {name:"Angularjs字符串插入26",link:"insert-string.html"},{name:"Angularjs脏检查$watch",link:"Angularjs.html"},
+        {name:"Angularjs加法机27",link:"adder.html"},
+        {name:"Angularjs购物车28",link:"shopping.html"},
+        {name:"Angularjs控制器嵌套29",link:"controller-nested.html"},
+        {name:"Angularjs表达式30",link:"expression.html"},
+        {name:"Angularjs计算器案例31",link:"calculator.html"},
+        {name:"Canvas时钟32",link:"clock.html"},
+        {name:"Angularjs字符串插入33",link:"insert-string.html"},
+        {name:"Angularjs计算器案例34",link:"calculator.html"},
+        {name:"Canvas时钟35",link:"clock.html"},
+        {name:"Angularjs字符串插入36",link:"insert-string.html"},{name:"Angularjs脏检查$watch",link:"Angularjs.html"},
+        {name:"Angularjs加法机37",link:"adder.html"},
+        {name:"Angularjs购物车38",link:"shopping.html"},
+        {name:"Angularjs控制器嵌套39",link:"controller-nested.html"},
+        {name:"Angularjs表达式40",link:"expression.html"},
+        {name:"Angularjs计算器案例41",link:"calculator.html"},
+        {name:"Canvas时钟42",link:"clock.html"},
+        {name:"Angularjs字符串插入43",link:"insert-string.html"},
+        {name:"Angularjs计算器案例44",link:"calculator.html"}
+    ];
+    return list;
+}
 
 
